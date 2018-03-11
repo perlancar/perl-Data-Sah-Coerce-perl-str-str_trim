6550306448e1e53c51a3107cd860b8d186508445
perlancar (on pc-home) <perlancar@gmail.com>
2018-03-11 10:14:47 +0700

    Initial commit

---
+++ lib/Data/Sah/Coerce/perl/str/str_ltrim.pm
@@ -0,0 +1,43 @@
+package Data::Sah::Coerce::perl::str::str_ltrim;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        v => 2,
+        enable_by_default => 0,
+        prio => 50,
+    };
+}
+
+sub coerce {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+
+    $res->{expr_match} = "!ref($dt)";
+    $res->{expr_coerce} = join(
+        "",
+        "do { my \$res = $dt; \$res =~ s/\\A\\s+//s; \$res }",
+    );
+
+    $res;
+}
+
+1;
+# ABSTRACT: Trim whitespaces at the beginning of string
+
+=for Pod::Coverage ^(meta|coerce)$
+
+=head1 DESCRIPTION
+
+The rule is not enabled by default. You can enable it in a schema using e.g.:
+
+ ["str", "x.perl.coerce_rules"=>["str_ltrim"]]
